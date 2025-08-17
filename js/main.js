7cc92987176c0a1f6c58f63d7045214b6bf22f95
Andrey Baev <abaev70@hotmail.com>
2025-08-18 04:00:23 +0700

    feat(menu): menu layout completed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   const burger = document.getElementById("burger");
-  const header = document.getElementsByClassName("header")[0];
+  console.log(burger);
+  const header = document.getElementById("header");
   console.log(header);
+
+  const mainNavLinks = document.getElementsByClassName("main-nav__link");
+  console.log(mainNavLinks);
 
   burger.addEventListener("click", function () {
     header.classList.toggle("header--open");
   });
 
+  for (let i = 0; i < mainNavLinks.length; i++) {
+    mainNavLinks[i].addEventListener("click", function () {
+      header.classList.remove("header--open");
+    });
+  }
 });
