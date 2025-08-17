fa2ecad283303b65850e706f941fbc6c57243f25
Andrey Baev <abaev70@hotmail.com>
2025-08-18 03:31:38 +0700

    feat(menu): add menu mobile adaptive

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
- document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
   const burger = document.getElementById("burger");
   const header = document.getElementsByClassName("header")[0];
   console.log(header);
 
-  burger.addEventListener("click", function(){
+  burger.addEventListener("click", function () {
     header.classList.toggle("header--open");
   });
 
- });
+});
