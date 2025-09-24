8e9884656f88c37c94cd56e965d380412d4d3e5d
AbaevB <abaev70@hotmail.com>
2025-09-24 16:25:34 +0700

    feat(main.js): header-nav toggle functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("DOM Loaded");
-});
+document.addEventListener('DOMContentLoaded', function() {
+  const burger = document.getElementById('burger');
+  const header = document.getElementById('header');
+  const headerLink = document.querySelectorAll('.main-nav__link');
+
+
+  burger.addEventListener('click', function() {
+    header.classList.toggle('header--active');
+  })
+
+ headerLink.forEach((item) => {
+    item.addEventListener('click', function(e) { // добавляем аргумент e
+      e.stopPropagation();                     // останавливаем всплытие события
+      header.classList.remove('header--active');
+    });
+  });
+
+})
